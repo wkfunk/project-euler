94573fdb182abb087189414dcb0225d0d9c26ca1
Keith Funkhouser <keith.funkhouser@gmail.com>
2011-12-01 16:04:08 -0500

    solutions to 20 and 21

--- Problem 20.R
+++ Problem 20.R
@@ -1,5 +1,36 @@
 ##############################Problem 20:##############################
 # Find the sum of the digits in the number 100!
-digg <- sprintf("%.0f", factorial(100))
-sum(as.numeric(substring(digg, 1:nchar(digg), 1:nchar(digg))))
-# [1] 762
+#digg <- sprintf("%.0f", factorial(100))
+#sum(as.numeric(substring(digg, 1:nchar(digg), 1:nchar(digg))))
+## [1] 762
+
+sumDigitsofNFactorial  <- function(n) {
+  digits <- c(4,2);
+  
+  for (multiplier in 5:n) {
+    digits <- multiplier*digits;
+    if (any(digits>9)) {
+      for (ii in 1:(length(digits)-1)) {
+        new <- digits[ii]%%10
+        digits[ii+1] <- digits[ii+1] + ((digits[ii]-new)/10)
+        digits[ii] <- new
+      }
+      ii  <- (ii + 1)
+      while (digits[ii] > 9) {
+        new  <- digits[ii]%%10
+        digits  <- append(digits,((digits[ii]-new)/10))
+        digits[ii]  <- new
+        ii  <-  (ii + 1)
+      }
+    }
+  }
+  return (sum(digits));
+}
+
+
+#> system.time(sumDigitsofNFactorial(100))
+ #user  system elapsed 
+#0.052   0.000   0.052 
+#> sumDigitsofNFactorial(100)
+#[1] 648
+  
--- Problem 21.R
+++ Problem 21.R
@@ -16,15 +16,92 @@ findPrimeFactors(sumz)
 # }
 sumProperDivisors <- function( input ) {
   nah <- 1:ceiling(input/2)
-  sum(nah[which((input/nah) %% 1 == 0)])
+  return(sum(nah[which((input/nah) %% 1 == 0)]))
 }
-slick <- array(,c(10,3))
-slick[ ,1] <- 1:dim(slick)[1]
-for (ii in 1:dim(slick)[1]) {
-  slick[ii,2] <- sumProperDivisors(digg[ii])
+
+#####################################
+#new method
+#1. enumerate prime factors
+#2. generate all possible subsets of prod(primeFactors)
+
+#e.g. prime factors of 220  -> 2,2,5,11
+#then, the possible subsets of these are:
+#11, 5, (5*11), 2, (2*11), (2*5), (2*5*11), etc.
+
+#this function was sourced from problem 3 original solution, and can be used to find the prime factors of a number.
+findPrimeFactors <- function(num) {
+  primeFac <- c()
+  while (num>1){
+    if (num%%2 ==0) {
+      primeFac <- append(primeFac, 2)
+      num <- num/2
+    }
+    else {
+      seed <- 3
+      while (num%%seed != 0) {
+        seed <- seed+2
+      }
+      primeFac <- append(primeFac, seed)
+      num <- num/seed
+    }
+  }
+  return(primeFac)
+}
+
+#this function returns the sum of the divisors below input
+sumFactors  <- function(input) {
+  x <- findPrimeFactors(input)
+  #x <- sort(x)
+  counts  <- as.vector(table(x) + 1)
+  uniq <- unique(x)
+
+  y <- rep(1,prod(counts))
+  meep <- 0:(prod(counts)-1)
+
+  perSets <- c()
+  for(jj in 1:length(uniq)) {
+    perSets  <- append(perSets, max(1,prod(counts[(jj+1):length(counts)]), na.rm=TRUE))
+  }    
+
+  for (ii in 1:length(y)){
+    for (jj in 1:length(uniq)) {
+      y[ii] <- y[ii]*((uniq[jj])**floor(meep[ii]/perSets[jj]))
+      meep[ii] <- meep[ii]%%perSets[jj]
+    }
+  }
+  return(sum(y[-length(y)]))
+}
+
+#wow, this function is actually pretty terrible...
+y <- 2:10000;
+system.time( for (ii in 1:length(y)) { y[ii] <- sumFactors(y[ii])})
+   #user  system elapsed 
+ #13.267   0.003  13.269 
+
+#original function was not as bad
+y <- 2:10000;
+system.time( for (ii in 1:length(y)) { y[ii] <- sumProperDivisors(y[ii])})
+   #user  system elapsed 
+  #1.110   0.003   1.112
+
+
+#now, simply compute the sum of divisors for all numbers <= 10000
+sumAmicNums  <- function(highest) {
+  digg  <- 1:highest
+  for (ii in 1:length(digg)) {
+    digg[ii] <- sumProperDivisors(digg[ii])
   }
   
-h = hash(digg, slick)
-for (ii in 1:dim(slick)[1]) {
-  slick[ii,3] <- slick[ii,2] ==
+  amics <- c()
+
+  for (ii in 1:length(digg)) {
+    if (!is.na(digg[digg[ii]]) & ii != digg[ii] &  ii == digg[digg[ii]]) {
+     amics <- append(amics, digg[ii])
+    } 
   }
+  
+  return(sum(amics))
+}
+
+sumAmicNums(10000)
+#[1] 31626
